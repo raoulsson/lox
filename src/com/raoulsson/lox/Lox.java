af6fceece80e5838481d7889a621abf2c18216ad
raoulsson <raoul.schmidiger@gmail.com>
2022-05-23 22:13:22 +0800

    Parser impl
    Took 1 hour 6 minutes

--- src/com/raoulsson/lox/Lox.java
+++ src/com/raoulsson/lox/Lox.java
@@ -53,14 +53,20 @@ public class Lox {
         }
     }
 
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, "at '" + token.lexeme + "'", message);
+        }
+    }
+
     static void error(int line, String message) {
         report(line, "", message);
     }
 
-    private static void report(int line, String where,
-                               String message) {
-        System.err.println(
-                "[line " + line + "] Error" + where + ": " + message);
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
     }
 
